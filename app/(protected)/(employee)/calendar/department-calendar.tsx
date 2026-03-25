869a672c3bb70544283f1317a9387c113fdaed48
Ericson Castasus <castasuse@gmail.com>
2026-03-25 17:08:27 +0800

    department calendar and holidays

---
+++ app/(protected)/(employee)/calendar/department-calendar.tsx
@@ -0,0 +1,49 @@
+import { getApprovedLeaveRequests } from "./actions"
+import CustomCalendar from "./components/custom-calendar"
+import { HolidayEvent } from "./components/custom-calendar"
+import { startOfMonth, setDate, addMonths } from "date-fns"
+import { fetchHolidaysAction } from "./actions"
+import { HolidayCard } from "./components/holiday-card"
+
+export default async function DepartmentCalendar() {
+  const leaves = await getApprovedLeaveRequests()
+  const holidays = await fetchHolidaysAction()
+
+  const formattedLeaves = leaves.map((leave) => ({
+    id: leave.id,
+    employeeName:
+      leave.employee_profiles.first_name +
+      " " +
+      leave.employee_profiles.last_name,
+    avatar_url: leave.employee_profiles.avatar_url,
+    type: leave.leave_type.leave_type,
+    duration: leave.duration,
+    startDate: new Date(leave.date),
+    endDate: new Date(leave.date),
+    status: leave.status.status_name,
+  }))
+
+  const formattedHolidays = holidays.map((holiday) => ({
+    id: holiday.name + holiday.date,
+    name: holiday.name,
+    date: new Date(holiday.date),
+  }))
+
+  return (
+    <div>
+      <CustomCalendar events={formattedLeaves} holidays={formattedHolidays} />
+    </div>
+  )
+}
+
+export async function Holidays() {
+  const holidays = await fetchHolidaysAction()
+
+  const formattedHolidays = holidays.map((holiday) => ({
+    id: holiday.name + holiday.date,
+    name: holiday.name,
+    date: new Date(holiday.date),
+  }))
+
+  return <HolidayCard holidays={formattedHolidays} />
+}
